ccda09e28f5e3956a64b5221fdf3429e90da6b56
Jimin Kim <geall4@naver.com>
2023-06-16 20:15:04 +0900

    Change mongoose-encryption to crypto-js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+var SHA256 = require("crypto-js/sha256");
+
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
@@ -15,9 +16,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-var secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] }); //model 생성전에 만들기
 
 const User = new mongoose.model('User', userSchema)
 
@@ -36,7 +34,7 @@ app.get("/register", (req, res) => {
 app.post('/register', async (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: SHA256(req.body.password).toString()
     });
 
     await newUser.save().then((user) => {
@@ -50,7 +48,7 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) =>
     await User.findOne({ email: req.body.username }).then((foundUser) => {
         if (foundUser) {
-            if (foundUser.password === req.body.password) {
+            if (foundUser.password === SHA256(req.body.password).toString()) {
                 res.render('secrets');
             } else
                 res.send('not correct password');
